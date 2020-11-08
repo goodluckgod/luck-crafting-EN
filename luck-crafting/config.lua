91b27c5d704b44c59e2b7b216110ca463db03cf9
doğu <42766561+goodluckgod@users.noreply.github.com>
2020-11-08 15:40:15 +0300

    Update config.lua

--- luck-crafting/config.lua
+++ luck-crafting/config.lua
@@ -1,32 +1,32 @@
 Config = {}
 
-Config.BenchModel = 'prop_tool_bench02_ld' -- Bench modeli.
+Config.BenchModel = 'prop_tool_bench02_ld' -- Bench model.
 
-Config.isDisc = false -- Disc kullanıyorsanız bunu true yapınız.
+Config.isDisc = false -- if you use disc set true
 
 Config.Crafts = {
     ['Craft1'] = { 
-        coords = vector3(461.08, -981.27, 29.69), -- Craft sisteminin yeri.
-        heading = 30, -- Crafting Bench'in açısı.
-        msg = 'Craftlamak için [E]', -- 3D text içerisinde çıkacak yazı.
+        coords = vector3(461.08, -981.27, 29.69), -- Posistion of crafting.
+        heading = 30, -- Heading of bench.
+        msg = 'Craftlamak için [E]', -- Text of 3DDrawText.
     },
 }
 
 Config.CraftItems = {
     ['Telefon'] = { 
-        item = {name = "WEAPON_PISTOL", count = 5}, -- Count eşyasa eşyanın sayısını silahsa mersini belirler. (Disc için her türlü eşya veya silahın sayısını belirler.)
-        minRep = 100, -- Gereken beceriyi belirler.
-        giveRep = 50, -- Eşya craftlandığında verilecek beceriyi belirler
+        item = {name = "WEAPON_PISTOL", count = 5}, -- Crafted Item
+        minRep = 100, -- Minumum rep for crafting item
+        giveRep = 50, -- That number will gived to you craft when you craft (Respect)
         dependencies = {
-        [1] = {item = "water", count = 1}, -- Slot 1, item eşyanın ismi count ise kaç tane gerektiği (nil koyarsanız slotun boş olması gerektiğini tanımlatırsınız.).
-        [2] = nil, -- Slot 2, item eşyanın ismi count ise kaç tane gerektiği (nil koyarsanız slotun boş olması gerektiğini tanımlatırsınız.).
-        [3] = nil, -- Slot 3, item eşyanın ismi count ise kaç tane gerektiği (nil koyarsanız slotun boş olması gerektiğini tanımlatırsınız.).
-        [4] = nil, -- Slot 4, item eşyanın ismi count ise kaç tane gerektiği (nil koyarsanız slotun boş olması gerektiğini tanımlatırsınız.).
-        [5] = nil, -- Slot 5, item eşyanın ismi count ise kaç tane gerektiği (nil koyarsanız slotun boş olması gerektiğini tanımlatırsınız.).
-        [6] = nil, -- Slot 6, item eşyanın ismi count ise kaç tane gerektiği (nil koyarsanız slotun boş olması gerektiğini tanımlatırsınız.).
-        [7] = nil, -- Slot 7, item eşyanın ismi count ise kaç tane gerektiği (nil koyarsanız slotun boş olması gerektiğini tanımlatırsınız.).
-        [8] = nil, -- Slot 8, item eşyanın ismi count ise kaç tane gerektiği (nil koyarsanız slotun boş olması gerektiğini tanımlatırsınız.).
-        [9] = {item = "disc_ammo_pistol", count = 1}, -- Slot 9, item eşyanın ismi count ise kaç tane gerektiği (nil koyarsanız slotun boş olması gerektiğini tanımlatırsınız.).
+        [1] = {item = "water", count = 1}, -- Slot 1, item is name of your item, count is your count of your item (if is nil slot will be accepted empty).
+        [2] = nil, -- Slot 2 item is name of your item, count is your count of your item (if is nil slot will be accepted empty).
+        [3] = nil, -- Slot 3 item is name of your item, count is your count of your item (if is nil slot will be accepted empty).
+        [4] = nil, -- Slot 4 item is name of your item, count is your count of your item (if is nil slot will be accepted empty).
+        [5] = nil, -- Slot 5 item is name of your item, count is your count of your item (if is nil slot will be accepted empty).
+        [6] = nil, -- Slot 6 item is name of your item, count is your count of your item (if is nil slot will be accepted empty).
+        [7] = nil, -- Slot 7, item is name of your item, count is your count of your item (if is nil slot will be accepted empty).
+        [8] = nil, -- Slot 8 item is name of your item, count is your count of your item (if is nil slot will be accepted empty).
+        [9] = {item = "disc_ammo_pistol", count = 1}, -- Slot 9 item is name of your item, count is your count of your item (if is nil slot will be accepted empty).
         },
     },
 }
