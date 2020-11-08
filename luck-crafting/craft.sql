f06c4b1d6ae3fd240a3dec8a00795c0331578bef
doğu <42766561+goodluckgod@users.noreply.github.com>
2020-11-08 17:25:40 +0300

    Update craft.sql

--- luck-crafting/craft.sql
+++ luck-crafting/craft.sql
@@ -11,52 +11,11 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- tablo yapısı dökülüyor essentialmode.phone_app_chat
-CREATE TABLE IF NOT EXISTS `phone_app_chat` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `channel` varchar(20) NOT NULL,
-  `message` varchar(255) NOT NULL,
-  `time` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=268 DEFAULT CHARSET=utf8;
-
--- Veri çıktısı seçilmemişti
-
--- tablo yapısı dökülüyor essentialmode.phone_calls
-CREATE TABLE IF NOT EXISTS `phone_calls` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `owner` varchar(10) NOT NULL,
-  `num` varchar(10) NOT NULL,
-  `incoming` int(11) NOT NULL,
-  `time` timestamp NOT NULL DEFAULT current_timestamp(),
-  `accepts` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16356 DEFAULT CHARSET=utf8;
-
--- Veri çıktısı seçilmemişti
-
--- tablo yapısı dökülüyor essentialmode.phone_messages
-CREATE TABLE IF NOT EXISTS `phone_messages` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `transmitter` varchar(10) NOT NULL,
-  `receiver` varchar(10) NOT NULL,
-  `message` varchar(255) NOT NULL DEFAULT '0',
-  `time` timestamp NOT NULL DEFAULT current_timestamp(),
-  `isRead` int(11) NOT NULL DEFAULT 0,
-  `owner` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=45249 DEFAULT CHARSET=utf8;
-
--- Veri çıktısı seçilmemişti
-
--- tablo yapısı dökülüyor essentialmode.phone_users_contacts
-CREATE TABLE IF NOT EXISTS `phone_users_contacts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `identifier` varchar(60) CHARACTER SET utf8mb4 DEFAULT NULL,
-  `number` varchar(10) CHARACTER SET utf8mb4 DEFAULT NULL,
-  `display` varchar(64) CHARACTER SET utf8mb4 NOT NULL DEFAULT '-1',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=992 DEFAULT CHARSET=utf8;
+-- tablo yapısı dökülüyor essentialmode.luck_crafting
+CREATE TABLE IF NOT EXISTS `luck_crafting` (
+  `identifier` varchar(50) DEFAULT NULL,
+  `reputation` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Veri çıktısı seçilmemişti
 
